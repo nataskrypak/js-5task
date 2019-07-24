16e1e5698123b3ccb75550e0bf48c85bb1a05c03
nataskrypak <zakaznataiua@gmail.com>
2019-07-24 17:23:37 +0000

    sample code

---
+++ js/index.js
@@ -0,0 +1,19 @@
+alert('Hello Javascript!');
+
+let userName = prompt('enter your name');
+let greetings = 'Hello! ';
+alert(userName);
+let userAge = parceFloat(prompt('how old are you?'));
+alert(userAge);
+let nextAge = add(userAge + 1);
+alert('наступого вам буде: ' +nextAge);
+if(userAge) >= 18{ // > < <= >= != !== || &&
+alert('ви вже повнолытный');
+} else {
+alert('ви ще не повнолітній');
+}
+function add = (a, b); {
+    let result a + b;
+    return result;
+
+}
